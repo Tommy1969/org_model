81171444d3e920640fc096f0064c11c7adfeda91
Tommy <tokutomi@degino.com>
2021-07-22 10:16:14 +0900

    chore: 初期化した。

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-/**
- * TypeDoc の確認用
- */
-const main = () => {
-  console.log('Hello world!')
-}
--- src/repository/db.ts
+++
@@ -1,8 +0,0 @@
-import { Client } from "pg";
-
-export const client = new Client({
-  user: process.env.DB_USER,
-  database: process.env.DB_SPACE,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT)
-});
