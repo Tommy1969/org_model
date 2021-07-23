17f00f8d456b9e75b5cfafbc5cfbdc787f8e2483
Tommy <tokutomi@degino.com>
2021-07-23 19:39:18 +0900

    refactor: Query ビルダーに切り替えた。

--- src/repository/org_table.spec.ts
+++ src/repository/org_table.spec.ts
@@ -1,8 +1,8 @@
 import { client } from "./db";
 import { NotFoundError } from '../exception'
+import { CATEGORY } from '../model/node.interface'
 import { Company, Department, Facility } from '../model/node'
-import { NodeType } from '../model/node.interface'
-import {OrgTable, CmpTable, DepTable, FacTable} from './org_table'
+import { CmpTable, DepTable, FacTable } from './org_table'
 
 const TEST_DATA = [
   {id:'c01', category:1, name:'会社1', parent:null},
@@ -18,17 +18,8 @@ mockQuery.mockImplementation((query:string, arg:string[]) => {
   return {rowCount: result.length, rows:result}
 })
 
-describe('org テーブル操作について', () => {
-  const target = new OrgTable(client)
-  let result:NodeType
-  
-  it('ID でモデルを返すこと', async () => {
-    result = await target.getById('c01')
-    expect(result.name).toEqual('会社1')
-  })
-  it('存在しなければ例外になること', async () => {
-    await expect(target.getById('c99')).rejects.toThrow(NotFoundError)
-  })
+beforeEach(() => {
+  mockQuery.mockClear()
 })
 
 describe('会社のテーブル操作について', () => {
@@ -37,6 +28,7 @@ describe('会社のテーブル操作について', () => {
   it('ID で会社オブジェクトを返すこと', async () => {
     const exp:Company = new Company({id: 'c01', name: '会社1'})
     const result = await target.getById('c01')
+    expect(mockQuery).toBeCalledWith(expect.any(String), ['c01', CATEGORY.COMPANY])
     expect(result.data).toMatchObject(exp.data)
   })
   it('存在しなければ例外になること', async () => {
@@ -50,6 +42,7 @@ describe('部門のテーブル操作について', () => {
   it('ID で部門オブジェクトを返すこと', async () => {
     const exp:Department = new Department({id: 'd0121', name: '部門121', parent: 'c01'})
     const result = await target.getById('d0121')
+    expect(mockQuery).toBeCalledWith(expect.any(String), ['d0121', CATEGORY.DEPARTMENT])
     expect(result.data).toMatchObject(exp.data)
   })
   it('存在しなければ例外になること', async () => {
@@ -63,6 +56,7 @@ describe('施設のテーブル操作について', () => {
   it('ID で施設オブジェクトを返すこと', async () => {
     const exp:Department = new Facility({id: 'f0131', name: '施設131', parent: 'c01'})
     const result = await target.getById('f0131')
+    expect(mockQuery).toBeCalledWith(expect.any(String), ['f0131', CATEGORY.FACILITY])
     expect(result.data).toMatchObject(exp.data)
   })
   it('存在しなければ例外になること', async () => {
--- src/repository/org_table.ts
+++ src/repository/org_table.ts
@@ -1,18 +1,25 @@
 import { Client } from "pg";
 import { NotFoundError } from '../exception'
 import { Company, Department, Facility } from '../model/node'
-import { NodeType } from '../model/node.interface'
+import { CATEGORY, NodeType } from '../model/node.interface'
+import { Query } from "../util/query";
 
 export class OrgTable {
-  #client
+  readonly #client
+  readonly #baseQuery:Query = new Query({
+    tables:   ['org'],
+    filters:  ['disabled=false'],
+    orders:   ['created_at']
+  })
 
   constructor(client:Client) {
     this.#client = client
   }
 
-  async getById(id:string):Promise<NodeType> {
-    const query = 'select * from org where id=$1;'
-    const result = await this.#client.query(query, [id])
+  async getById(id:string, categoly:CATEGORY):Promise<NodeType> {
+    const query = this.#baseQuery.spawn({filters: ['id=$1', 'category=$2']}).getSelect();
+    const result = await this.#client.query(query, [id, categoly])
+
     if (result.rowCount === 0) {
       throw new NotFoundError(id)
     }
@@ -25,7 +32,7 @@ export class OrgTable {
  */
 export class CmpTable extends OrgTable {
   async getById(id:string):Promise<Company> {
-    return new Company(await super.getById(id));
+    return new Company(await super.getById(id, CATEGORY.COMPANY));
   }
 }
 
@@ -34,7 +41,7 @@ export class CmpTable extends OrgTable {
  */
 export class DepTable extends OrgTable {
   async getById(id:string):Promise<Department> {
-    return new Department(await super.getById(id));
+    return new Department(await super.getById(id, CATEGORY.DEPARTMENT));
   }
 }
 
@@ -43,6 +50,6 @@ export class DepTable extends OrgTable {
  */
 export class FacTable extends OrgTable {
   async getById(id:string):Promise<Facility> {
-    return new Facility(await super.getById(id));
+    return new Facility(await super.getById(id, CATEGORY.FACILITY));
   }
 }
