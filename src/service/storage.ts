d8c35e8e65751d829c6f5840af4e7fc80f19de1c
Tommy <tokutomi@degino.com>
2021-07-23 20:31:00 +0900

    feat: サービスに部門、施設を追加した。

---
+++ src/service/storage.ts
@@ -0,0 +1,20 @@
+import { client } from "../repository/db";
+import { CmpTable, DepTable, FacTable } from '../repository/org_table'
+import { Company, Department, Facility } from '../model/node'
+
+export class StorageService {
+  static async getCompanyById(id:string):Promise<Company> {
+    const tbl = new CmpTable(client);
+    return await tbl.getById(id)
+  }
+
+  static async getDepartmentById(id:string):Promise<Department> {
+    const tbl = new DepTable(client);
+    return await tbl.getById(id)
+  }
+
+  static async getFacilityById(id:string):Promise<Facility> {
+    const tbl = new FacTable(client);
+    return await tbl.getById(id)
+  }
+}
